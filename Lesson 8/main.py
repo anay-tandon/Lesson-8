527929dfeaf0984b5ae702d859fec12e169de297
Anay Tandon <uk.anay.tandon@gmail.com>
2025-09-10 18:58:11 +0100

    Add files via upload

---
+++ Lesson 8/main.py
@@ -0,0 +1,27 @@
+import pgzrun
+from random import randint
+from time import time
+
+WIDTH = 800
+HEIGHT = 600
+
+TITLE = "Connecting Satellites"
+
+satellites = []
+lines = []
+next_satellite = 0
+
+start_time = 0
+end_time = 0
+total_time = 0
+
+number_of_satellite = 8
+
+def create_satellites():
+    global start_time
+    for count in range(0, number_of_satellite):
+        satellite = Actor("satellite")
+        satellite.pos = (randint(40, WIDTH - 40), randint(40, HEIGHT - 40))
+        satellites.append(satellite)
+    start_time = time()
+print(start_time)
